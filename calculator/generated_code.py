b297fc3b2868f59b0c213295fb4702949c4143ab
Prakhar Jagwani <54989891+Prakhar314@users.noreply.github.com>
2023-10-17 21:46:54 +0000

    working app

--- calculator/generated_code.py
+++ calculator/generated_code.py
@@ -1,11 +1,18 @@
 main.cpp
 ```
-#include <iostream>
 #include "calculator.h"
+#include <iostream>
+#include <string>
 
 int main() {
+    std::string input;
+    std::getline(std::cin, input);
+
     Calculator calculator;
-    calculator.run();
+    double result = calculator.calculate(input);
+
+    std::cout << "Result: " << result << std::endl;
+
     return 0;
 }
 ```
@@ -15,9 +22,11 @@ calculator.h
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+#include <string>
+
 class Calculator {
-public:
-    void run();
+    public:
+        double calculate(std::string input);
 };
 
 #endif
@@ -25,44 +34,118 @@ public:
 
 calculator.cpp
 ```
-#include <iostream>
 #include "calculator.h"
+#include <iostream>
+#include <stack>
+#include <vector>
 
-void Calculator::run() {
-    double num1, num2;
-    char op;
-
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-
-    std::cout << "Enter operator (+, -, *, /): ";
-    std::cin >> op;
-
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
-
-    double result;
-
+double evaluate(double a, double b, char op) {
     switch (op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        default:
-            std::cout << "Invalid operator";
-            return;
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        default: return 0;
+    }
+}
+
+bool is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int get_precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+std::vector<std::string> tokenize(const std::string& input) {
+    std::vector<std::string> tokens;
+    std::string token;
+
+    for (size_t i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        if (c == ' ') {
+            if (!token.empty()) {
+                tokens.push_back(token);
+                token.clear();
+            }
+        } else if (is_operator(c)) {
+            if (!token.empty()) {
+                tokens.push_back(token);
+                token.clear();
+            }
+            tokens.push_back(std::string(1, c));
+        } else {
+            token += c;
+        }
     }
 
-    std::cout << "Result: " << result << std::endl;
+    if (!token.empty()) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+double Calculator::calculate(std::string input) {
+    std::vector<std::string> tokens = tokenize(input);
+    std::stack<double> values;
+    std::stack<char> operators;
+
+    for (size_t i = 0; i < tokens.size(); i++) {
+        std::string token = tokens[i];
+
+        if (token.length() == 1 && is_operator(token[0])) {
+            while (!operators.empty() && is_operator(operators.top()) &&
+                   get_precedence(operators.top()) >= get_precedence(token[0])) {
+                double b = values.top();
+                values.pop();
+                double a = values.top();
+                values.pop();
+                char op = operators.top();
+                operators.pop();
+                double result = evaluate(a, b, op);
+                values.push(result);
+            }
+            operators.push(token[0]);
+        } else {
+            double value = std::stod(token);
+            values.push(value);
+        }
+    }
+
+    while (!operators.empty()) {
+        double b = values.top();
+        values.pop();
+        double a = values.top();
+        values.pop();
+        char op = operators.top();
+        operators.pop();
+        double result = evaluate(a, b, op);
+        values.push(result);
+    }
+
+    return values.top();
 }
 ```
 
-This concludes a fully working implementation.
+This implementation uses the Shunting Yard Algorithm to convert the input expression into Reverse Polish Notation (RPN), which is then evaluated using a stack. The `tokenize` function splits the input into tokens, which can be either numbers or operators. The `calculate` method takes an input string, tokenizes it, converts it into RPN, and then evaluates it using the stack. 
+
+Note: This implementation assumes that the input is well-formed and does not handle error cases such as division by zero.
+
+CMakeLists.txt
+```
+cmake_minimum_required(VERSION 3.10)
+
+project(calculator)
+
+add_executable(calculator main.cpp calculator.cpp)
+```
+
+this concludes a fully working implementation.
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 
 from dotenv import load_dotenv
 
+DEBUG = False
+
 def read_prompt_from_dir(prompt_dir: str) -> str:
     prompt = ""
     with open(os.path.join(prompt_dir, "prompt"), "r") as f:
@@ -27,12 +29,40 @@ def colored_input(text: str, color: str = "red", end: str = "") -> str:
     print_colored(text, color, end=end)
     return input()
 
+def save_files(project_dir: str, output:str):
+    lines = output.split("\n")
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("```") and i>0:
+            filename = os.path.join(project_dir,lines[i-1].strip())
+            # create directory if it doesn't exist
+            if not os.path.exists(os.path.dirname(filename)):
+                os.makedirs(os.path.dirname(filename))
+            # collect code
+            j = i+1
+            code = ""
+            while j < len(lines):
+                if lines[j].startswith("```"):
+                    break
+                else:
+                    code += lines[j] + "\n"
+                j += 1
+            i = j + 1
+
+            # write code to file
+            with open(filename, "w") as f:
+                f.write(code)
+        else:
+            i += 1
+
+
 class EvaDB:
     def __init__(self, reset=False):
         self.cursor = evadb.connect().cursor()
         if reset:
             self.drop_table()
         self.cursor.query("CREATE TABLE IF NOT EXISTS prompts (project TEXT(20) UNIQUE, prompt TEXT(20));").execute()
+        self.cursor.query("CREATE FUNCTION IF NOT EXISTS TunedGPT IMPL 'chatgpt_tuned.py';").execute()
 
     def get_prompt(self, project: str) -> str:
         query = f"""SELECT prompt FROM prompts WHERE project = \"{project}\";"""
@@ -45,8 +75,7 @@ class EvaDB:
     def insert_prompt(self, project: str, prompt: str):
         # escape single quotes
         prompt = prompt.replace("'", "\'")
-        # # swap newlines for spaces
-        # prompt = prompt.replace("\n", " ")
+        
         query = ""
         if self.get_prompt(project) != "":
             query = f"""DELETE FROM prompts WHERE project = \"{project}\";"""
@@ -64,20 +93,15 @@ class EvaDB:
         self.cursor.drop_table("prompts").execute()
 
     def ask_gpt(self, prompt:str, user_input: str) -> str:
-        print_colored("Asking GPT...", "yellow")
-
         # swap single quotes with double quotes
         prompt = prompt.replace("\'", "\"")
         user_input = user_input.replace("\'", "\"")
 
-        # # swap newlines for spaces
-        # prompt = prompt.replace("\n", " ")
-        # user_input = user_input.replace("\n", " ")
+        query = f"SELECT TunedGPT(\'{user_input}\', \'{prompt}\');"
+        if DEBUG:
+            print_colored(query, "yellow")
 
-        query = f"SELECT ChatGPT(\'{prompt}\', \'{user_input}\');"
-        print_colored(query, "yellow")
-
-        return self.cursor.query(query).df()["chatgpt.response"][0]
+        return self.cursor.query(query).df()["tunedgpt.response"][0]
 
     def get_preprompt(self, preprompt:str)->str:
         f = open(os.path.join("pre_prompts", preprompt), "r")
@@ -97,37 +121,35 @@ if __name__ == "__main__":
     #Insert prompt into database
     evaDB.insert_prompt(project_dir, prompt)
 
-    gpt_query = evaDB.get_preprompt("clarify_start") 
-    user_input = evaDB.get_prompt(project_dir)
+    gpt_query = evaDB.get_preprompt("clarify_req") 
+    user_input = "Project Description: \n" + evaDB.get_prompt(project_dir)
 
-    #Get prompt from database
     while True:
         gpt_ans = evaDB.ask_gpt(gpt_query, user_input)
-        print_colored(gpt_ans, "yellow")
+        print_colored(gpt_ans, "green")
         if gpt_ans.lower().startswith("nothing"):
-            gpt_query = ""
             break
-        else:
-            print_colored(gpt_ans, "green")
 
         # check if the user wants to add a clarification
-        clarification = colored_input("Add clarification? (y/n): ")
-        if clarification == "y":
+        clarification = colored_input("Clarify? (y/n): ")
+        if clarification.lower() == "y" or clarification.lower() == "yes":
             clarification = colored_input("Enter clarification: ")
-            gpt_query = evaDB.get_preprompt("clarify_step")
-            user_input = clarification
+            user_input += "\n" + gpt_ans + "\n" + clarification
+            gpt_query = evaDB.get_preprompt("clarify_opt") 
         else:
             gpt_query = evaDB.get_preprompt("clarify_end")
             break
     
     # generate code
+    print_colored("Generating code...", "yellow")
     gpt_query = gpt_query + evaDB.get_preprompt("generate").replace("FILE_FORMAT", evaDB.get_preprompt("file_format"))
-    user_input = prompt
 
     gpt_ans = evaDB.ask_gpt(gpt_query, user_input)
     print_colored(gpt_ans, "green")
 
-    # save code to file
-    with open(os.path.join(project_dir, "generated_code.py"), "w") as f:
-        f.write(gpt_ans)
+    # ask user if they want to save the code
+    save = colored_input("Save code? (y/n): ")
+    if save.lower() == "y" or save.lower() == "yes":
+        save_files(project_dir, gpt_ans)
+        print_colored("Code saved!", "red")
     
